4f0efd74ca3939d7a7271445e385cc5026177835
Tapasweni <tapaswenipathak@gmail.com>
2014-06-10 19:29:48 +0530

    Template for C Codes Commit

---
+++ Template.c
@@ -0,0 +1,40 @@
+//Tapasweni Pathak
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+#define S(n)                        	scanf("%d",&n)
+#define SC(n)                       	scanf("%c",&n)
+#define SL(n)                       	scanf("%lld",&n)
+#define SF(n)                       	scanf("%lf",&n)
+#define SS(n)                       	scanf("%s",n)
+#define ST(n,m)			        scanf("%d %d",&n,&m);
+#define SLT(n,m)		        scanf("%lld %lld",&n,&m)
+
+#define P(n)			        printf("%d\n",n)
+#define PL(n)			        printf("%lld\n",n)
+#define PT(n,m)			        printf("%d %d",n,m)
+
+#define FORALL(i,a,b)               	for(int i=a;i<b;++i)
+#define FOREACH(i,a,b)		        for(int i=a;i<=b;++i)
+
+#define MAX(a,b)                    	( (a) > (b) ? (a) : (b))
+#define MIN(a,b)                    	( (a) < (b) ? (a) : (b))
+#define CHECKBIT(n,b)               	( (n >> b) & 1)
+#define ODD(a)                      	(a&1?1:0)
+#define EVEN(a)                     	(a&1?0:1)
+
+#define MOD                         	1000000007
+#define PI                          	3.1415925535897932384626433832795
+
+#define lli                         	long long int
+#define ulli                        	unsigned long long int
+
+int main()
+{
+ 
+    return 0;
+}
+
