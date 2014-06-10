51e21b5c092dde2d90f47d319bb8b43d9486d528
Tapasweni <tapaswenipathak@gmail.com>
2014-06-10 19:30:41 +0530

    Template for C++ Codes Commit

---
+++ Template.cpp
@@ -0,0 +1,47 @@
+//Tapasweni Pathak
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cctype>
+#include <map>
+#include <stack>
+#include <queue>
+#include <set>
+
+#define S(n)					scanf("%d",&n)
+#define SC(n)					scanf("%c",&n)
+#define SL(n)					scanf("%lld",&n)
+#define SF(n)					scanf("%lf",&n)
+#define SS(n)					scanf("%s",n)
+#define ST(n,m)					scanf("%d %d",&n,&m);
+#define SLT(n,m)				scanf("%lld %lld",&n,&m)
+
+#define P(n)					printf("%d\n",n)
+#define PL(n)					printf("%lld\n",n)
+#define PT(n,m)					printf("%d %d",n,m)
+
+#define FORALL(i,a,b)				for(int i=a;i<b;++i)
+#define FOREACH(i,a,b)				for(int i=a;i<=b;++i)
+
+#define MAX(a,b)				( (a) > (b) ? (a) : (b))
+#define MIN(a,b)				( (a) < (b) ? (a) : (b))
+#define CHECKBIT(n,b)				( (n >> b) & 1)
+#define ODD(a)					(a&1?1:0)
+#define EVEN(a)					(a&1?0:1)
+
+#define MOD					1000000007
+#define PI					3.1415925535897932384626433832795
+
+#define lli					long long int
+#define ulli					unsigned long long int
+using namespace std;
+
+int main()
+{
+    //ios_base::sync_with_stdio(0);
+
+    return 0;
+}
