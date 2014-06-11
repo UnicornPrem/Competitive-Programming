f9b2606fed487efb472960a4e4d9ae09b36e5439
Tapasweni <tapaswenipathak@gmail.com>
2014-06-11 17:00:14 +0530

    379A Code Commit

---
+++ CodeForces/379A.cpp
@@ -0,0 +1,44 @@
+
+//Tapasweni Pathak
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cctype>
+#include <map>
+#include <stack>
+#include <queue>
+#include <set>
+
+#define FORALL(i,a,b)				for(int i=a;i<b;++i)
+#define FOREACH(i,a,b)				for(int i=a;i<=b;++i)
+
+#define MAX(a,b)				( (a) > (b) ? (a) : (b))
+#define MIN(a,b)				( (a) < (b) ? (a) : (b))
+#define CHECKBIT(n,b)				( (n >> b) & 1)
+#define ODD(a)					(a&1?1:0)
+#define EVEN(a)					(a&1?0:1)
+
+#define MOD					1000000007
+#define PI					3.1415925535897932384626433832795
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+	int a, b, hours = 0, x = 0;
+	cin >> a >> b;
+	while (a){
+		hours += a;
+		x += a;
+		a = x / b;
+		x %= b;
+	}
+	cout << hours;
+
+
+    return 0;
+}
