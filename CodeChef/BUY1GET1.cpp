324639f7602f77360ed1f7322a19058493e52d6d
Tapasweni <tapaswenipathak@gmail.com>
2014-07-05 12:27:07 +0530

    BUY1GET1 Code Commit

---
+++ CodeChef/BUY1GET1.cpp
@@ -0,0 +1,55 @@
+
+//Tapasweni Pathak
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cctype>
+#include <map>
+#include <stack>
+#include <queue>
+#include <set>
+
+#define FORALL(i,a,b)				for(int i=a;i<b;++i)
+#define FOREACH(i,a,b)				for(int i=a;i<=b;++i)
+
+#define MAX(a,b)				( (a) > (b) ? (a) : (b))
+#define MIN(a,b)				( (a) < (b) ? (a) : (b))
+#define CHECKBIT(n,b)				( (n >> b) & 1)
+#define ODD(a)					(a&1?1:0)
+#define EVEN(a)					(a&1?0:1)
+
+#define MOD					1000000007
+#define PI					3.1415925535897932384626433832795
+
+#define lli					long long int
+#define ulli					unsigned long long int
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+	int T;
+	cin >> T;
+	while ( T--){
+		int cost = 0;
+		string S;
+		cin >> S;
+		sort (S.begin (), S.end () );
+		for (int i = 0; i < S.length (); ++i){
+			if (S[i] == S[i + 1]){
+				++i;
+				++cost;
+			}
+			else{
+				++cost;
+			}
+		}
+		cout << cost << endl;
+	}
+
+
+    return 0;
+}
