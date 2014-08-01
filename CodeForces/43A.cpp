f6496e2d2169aee89742bcd43ff384880f45e05b
Tapasweni <tapaswenipathak@gmail.com>
2014-08-01 20:25:31 +0530

    43A Code Commit

---
+++ CodeForces/43A.cpp
@@ -0,0 +1,94 @@
+
+//Tapasweni Pathak
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <cctype>
+#include <map>
+#include <stack>
+#include <queue>
+#include <set>
+#include <bitset>
+#include <deque>
+#include <list>
+
+#define FORALL(i,a,b)				for(int i = a; i < b; ++i)
+#define FOREACH(i,a,b)				for(int i = a; i <= b; ++i)
+#define REP(i,n) 		                for(int i = 0; i < n ; ++i)
+#define REVALL(i,n) 		                for(int i = n ; i >= 0 ; --i)
+#define REV(i,a,b) 		                for(int i = a ; i >= b ; --i)
+#define TR(c,i) 				for(typeof((c).begin()) i=(c).begin();	i!=(c).end(); i++)
+
+#define SC(n) 			                scanf("%d", &n)
+
+#define MAX(a,b)				( (a) > (b) ? (a) : (b))
+#define MIN(a,b)				( (a) < (b) ? (a) : (b))
+#define CHECKBIT(n,b)				( (n >> b) & 1)
+#define ODD(a)					(a & 1 ? 1 : 0)
+#define EVEN(a)					(a & 1 ? 0 : 1)
+
+#define MOD					1000000007
+#define PI					3.1415925535897932384626433832795
+#define INF 					1000000000 
+
+#define PP 					pair<int, int> 
+#define PB(a) 					push_back (a)
+#define ALL(v) 					v.begin () , v.end () 
+#define	VI    					vector<int> 
+#define MII					map<int, int> 
+#define MSI					map<string, int> 
+#define VII					vector<vector<int> > 
+#define VPP					vector<pair<int, int> > 
+#define VIL                 			vector<long long int>
+#define VIITER					vector<int>::iterator
+#define MAXVEC(a)				max_element (a.begin (), a.end ())
+#define MINVEC(a)				min_element (a.begin (), a.end ())
+
+#define LLI					long long int
+#define ULLI					unsigned long long int
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    /*
+    int T;
+    SC(T);
+    while (T--){
+    }
+    */
+	int n, team1_goals = 0, team2_goals = 0;
+	cin >> n;
+	string team[2];
+	string goals[n + 1];
+	cin >> goals[0];
+	team[0] = goals[0];
+	FORALL (i, 1, n){
+		cin >> goals[i];
+		if (goals[i] != team[0]){
+			team[1] = goals[i];
+		}
+	}
+
+	REP (i, n){
+		if (goals[i] == team[0]){
+			++team1_goals;
+		}
+		if (goals[i] == team[1]) {
+			++team2_goals;
+		}
+	}
+	
+	if (team1_goals > team2_goals){
+		cout << team[0];
+	}
+	else {
+		cout << team[1];
+	}
+
+
+    return 0;
+}
